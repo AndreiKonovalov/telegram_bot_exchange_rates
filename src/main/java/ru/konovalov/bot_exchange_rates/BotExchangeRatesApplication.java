4eab95a5e53a721107f58f99f4e2314fe126a1c8
Андрей Коновалов <akonovalov2015@mail.ru>
2023-06-09 13:41:19 +0500

    Initial commit

---
+++ src/main/java/ru/konovalov/bot_exchange_rates/BotExchangeRatesApplication.java
@@ -0,0 +1,13 @@
+package ru.konovalov.bot_exchange_rates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotExchangeRatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotExchangeRatesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/konovalov/bot_exchange_rates/BotExchangeRatesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.konovalov.bot_exchange_rates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotExchangeRatesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
